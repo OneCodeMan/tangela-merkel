37d2409e4eee170268a48ea81aeef6cd697bc98f
Dave Gumba <dave_gumba@Yahoo.com>
2020-05-10 17:37:36 -0400

    installed lodash for the purpose of querying objects better

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import strainCategories from '../../helpers/GenerateStrains';
+
+function Main() {
+    console.log(strainCategories);
+    return (
+        <div className="Main">
+          <h1>Hi :)</h1>
+        </div>
+      );
+}
+
+
+export default Main;
--- src/helpers/GenerateStrains.js
+++ src/helpers/GenerateStrains.js
@@ -1,25 +1,43 @@
 import strainsJSONString from './cannabis.json';
 
-let cannabisJSON = strainsJSONString.map(strain => JSON.parse(strain));
-cannabisJSON = strainsJSONString.filter(strain => strain.effects.includes('None') && strain.flavours.includes('None'));
-const aphrodisiacStrains = cannabisJSON.filter(strain => strain.effects.includes('Aphrodisiac') 
+let cannabisJSON = strainsJSONString.filter(strain => !strain.effects.includes('None') && !strain.flavours.includes('None'));
+
+let aphrodisiacStrains = cannabisJSON.filter(strain => strain.effects.includes('Aphrodisiac') 
                                                     || strain.effects.includes('Tingly'));
-const socialStrains = cannabisJSON.filter(strain => strain.effects.includes('Talkative') 
+aphrodisiacStrains = Object.values(aphrodisiacStrains);
+                                
+let socialStrains = cannabisJSON.filter(strain => strain.effects.includes('Talkative') 
                                               || strain.effects.includes('Giggly'));
-const appetiteStrains = cannabisJSON.filter(strain => strain.effects.includes('Hungry'));
-const creativeStrains = cannabisJSON.filter(strain => strain.effects.includes('Creative') 
+socialStrains = Object.values(socialStrains);
+
+let appetiteStrains = cannabisJSON.filter(strain => strain.effects.includes('Hungry'));
+appetiteStrains = Object.values(appetiteStrains);
+
+let creativeStrains = cannabisJSON.filter(strain => strain.effects.includes('Creative') 
                                                 || strain.effects.includes('Focused')
                                                 || strain.effects.includes('Energetic'));
-const productiveStrains = cannabisJSON.filter(strain => strain.effects.includes('Energetic')
+creativeStrains = Object.values(creativeStrains);
+
+let productiveStrains = cannabisJSON.filter(strain => strain.effects.includes('Energetic')
                                                   || strain.effects.includes('Focused'));
-const sorenessStrains = cannabisJSON.filter(strain => strain.effects.includes('Relaxed'));
-const depressionStrains = cannabisJSON.filter(strain => strain.effects.includes('Happy')
+productiveStrains = Object.values(productiveStrains);                                        
+
+let sorenessStrains = cannabisJSON.filter(strain => strain.effects.includes('Relaxed'));
+sorenessStrains = Object.values(sorenessStrains);
+
+let depressionStrains = cannabisJSON.filter(strain => strain.effects.includes('Happy')
                                                   || strain.effects.includes('Euphoric')
                                                   || strain.effects.includes('Uplifted'));
-const sleepStrains = cannabisJSON.filter(strain => strain.effects.includes('Sleepy'));
-const anxietyStrains = cannabisJSON.filter(strain => strain.effects.includes('Relaxed'));
+depressionStrains = Object.values(depressionStrains);
+
+let sleepStrains = cannabisJSON.filter(strain => strain.effects.includes('Sleepy'));
+sleepStrains = Object.values(sleepStrains);
+
+let anxietyStrains = cannabisJSON.filter(strain => strain.effects.includes('Relaxed'));
+anxietyStrains = Object.values(anxietyStrains);
 
 const strainCategories =  {
+  'allStrains': cannabisJSON,
   'aphrodisiacStrains': aphrodisiacStrains,
   'socialStrains': socialStrains,
   'appetiteStrains': appetiteStrains,
