d468ccac45c2fe18e0b311d3dc827b71d4a2624c
Dave Gumba <dave_gumba@Yahoo.com>
2020-05-10 14:31:12 -0400

    So I made arrays depending on purpose of usage, haven't tested them yet.

---
+++ datahandler/analyze.py
@@ -0,0 +1,1 @@
+# Analyzes the strains
--- src/helpers/GenerateStrains.js
+++ src/helpers/GenerateStrains.js
@@ -1,16 +1,34 @@
-import wordsJSONString from './words.json';
+import strainsJSONString from './cannabis.json';
 
-const wordsJSON = wordsJSONString.map(word => JSON.parse(word));
-const adjectives = wordsJSON.filter(word => word.type === 'adjective');
-const verbs = wordsJSON.filter(word => word.type === 'verb');
-const conjunctions = wordsJSON.filter(word => word.type === 'conjunction');
-const nouns = wordsJSON.filter(word => word.type === 'noun');
+let cannabisJSON = strainsJSONString.map(strain => JSON.parse(strain));
+cannabisJSON = strainsJSONString.filter(strain => strain.effects.includes('None') && strain.flavours.includes('None'));
+const aphrodisiacStrains = cannabisJSON.filter(strain => strain.effects.includes('Aphrodisiac') 
+                                                    || strain.effects.includes('Tingly'));
+const socialStrains = cannabisJSON.filter(strain => strain.effects.includes('Talkative') 
+                                              || strain.effects.includes('Giggly'));
+const appetiteStrains = cannabisJSON.filter(strain => strain.effects.includes('Hungry'));
+const creativeStrains = cannabisJSON.filter(strain => strain.effects.includes('Creative') 
+                                                || strain.effects.includes('Focused')
+                                                || strain.effects.includes('Energetic'));
+const productiveStrains = cannabisJSON.filter(strain => strain.effects.includes('Energetic')
+                                                  || strain.effects.includes('Focused'));
+const sorenessStrains = cannabisJSON.filter(strain => strain.effects.includes('Relaxed'));
+const depressionStrains = cannabisJSON.filter(strain => strain.effects.includes('Happy')
+                                                  || strain.effects.includes('Euphoric')
+                                                  || strain.effects.includes('Uplifted'));
+const sleepStrains = cannabisJSON.filter(strain => strain.effects.includes('Sleepy'));
+const anxietyStrains = cannabisJSON.filter(strain => strain.effects.includes('Relaxed'));
 
 const strainCategories =  {
-  'adjectives': adjectives,
-  'verbs': verbs,
-  'conjunctions': conjunctions,
-  'nouns': nouns,
+  'aphrodisiacStrains': aphrodisiacStrains,
+  'socialStrains': socialStrains,
+  'appetiteStrains': appetiteStrains,
+  'creativeStrains': creativeStrains,
+  'productiveStrains': productiveStrains,
+  'sorenessStrains': sorenessStrains,
+  'depressionStrains': depressionStrains,
+  'sleepStrains': sleepStrains,
+  'anxietyStrains': anxietyStrains,
 };
 
 export default strainCategories;
